8359b34260d84be6377d7618c773fd210897462a
Simon Guzman <lcdosguzman@gmail.com>
2024-03-02 14:34:17 -0300

    estrutura web

--- recruitradar/appUsers/urls.py
+++ recruitradar/appUsers/urls.py
@@ -3,4 +3,8 @@ from appUsers import views
 
 urlpatterns = [
     path('',views.home,name="Home"),
+    path('login',views.login,name="Login"),
+    path('registro',views.registro,name="Registro"),
+    path('aboutme',views.aboutme,name="Aboutme"),
+    path('contact',views.contact,name="Contact"),
 ]
--- recruitradar/appUsers/views.py
+++ recruitradar/appUsers/views.py
@@ -5,3 +5,15 @@ from appUsers.models import *
 # Create your views here.
 def home(request):
     return render(request,"appUsers/home.html")
+
+def login(request):
+    return render(request,"appUsers/login.html")
+
+def registro(request):
+    return render(request,"appUsers/registro.html")
+
+def aboutme(request):
+    return render(request,"appUsers/aboutme.html")
+
+def contact(request):
+    return render(request,"appUsers/contact.html")
